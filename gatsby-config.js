9276e0f2755f7de6449075e206451c1c95617ef5
Doubles078 <ddonohudb@gmail.com>
2018-09-05 16:38:26 -0400

    Remove news - last try

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,12 +12,6 @@ module.exports = {
         accessToken: process.env.CONTENTFUL_ACCESSTOKEN || ''
       }
      },
-     {
-      resolve: 'gatsby-plugin-mailchimp',
-      options: {
-        endpoint: 'https://senseibuff.us7.list-manage.com/subscribe/post?u=29eab8e6fef13e3f15a812696&amp;id=63285f69cf', // see instructions section below
-      },
-    },
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-remark',
     'gatsby-plugin-sass'
