d653ac05b462735a3ea98daec5b0dea3c0e05f88
Dan Donohue <dan.donohue@ef.com>
2018-09-06 11:07:20 -0400

    Removing yarn, updating packages.

--- src/components/postpage/newsLetter.js
+++ src/components/postpage/newsLetter.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import addToMailchimp from 'gatsby-plugin-mailchimp'
 
 export class NewsLetter extends Component {
   render () {
