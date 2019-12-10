7f1b51f64435a42f242df5d63687402de71c21ac
Dan Donohue <dan.donohue@EF.com>
2019-12-10 13:20:49 -0500

    feat: styles fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { TagProvider } from './src/components/context/TagContext'
+import './src/assets/styles/main.scss'
+
 export const wrapRootElement = ({ element }) => (
   <TagProvider>{element}</TagProvider>
 )
