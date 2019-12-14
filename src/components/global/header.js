a7f6703bd7c077f9c12917ce18cb8c07ded15654
doubles078 <ddonohudb@gmail.com>
2019-12-14 09:59:26 -0500

    feat: page title

--- src/components/global/header.js
+++ src/components/global/header.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
+import { Helmet } from 'react-helmet'
 import SlideMenu from './slideMenu'
 
 const Header = props => (
   <header className="header">
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>danvsworld 🌎</title>
+    </Helmet>
     <div className="wrapper">
       <h1>
         <Link to="/">danvsworld</Link>
